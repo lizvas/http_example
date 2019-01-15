b50f8aa449e7ed31c78b28fd2ba43f00ce6d7ba4
lizvas <42232426+lizvas@users.noreply.github.com>
2019-01-15 12:50:43 +0900

    Add files via upload
    read the comments; pay attention to the order of console.logs and how the terminal prints it out. order not the same...

---
+++ http_example.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+console.log('I did it!');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+//notice that console.log order and how it is typed
+//in the terminal, it outpts:
+// I did it!
+// I'm about to make the request!
+// I've made the request!
+// In response handler callback!
